017b5e0f41fbaec5dd265d43c861d981d8ac2b1b
cschol <cschol2112@gmail.com>
2012-05-15 20:32:36 -0600

    Removed  line endings.

--- init-custom.el
+++ init-custom.el
@@ -4,7 +4,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-enabled-themes (quote (zenburn)))
- '(custom-safe-themes (quote ("1f3c629dcdc50026cdf9d1d8fdc7173bc26848dc0bfd407f93c192bceaeeb27f" default))))
+ '(custom-safe-themes (quote ("1f3c629dcdc50026cdf9d1d8fdc7173bc26848dc0bfd407f93c192bceaeeb27f" default)))
+ '(org-agenda-files nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
